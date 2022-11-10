b6f37f3fd16cca4ca40ea045f76fff94b19b9fc0
Harrison Arranz-Hurtado <arranz-hurtado@HarrisonArranz-MacbookAir.local>
2022-11-09 21:44:41 -0500

    serving home.html

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const path = require('path');
+const app = express();
 
+app.use('/static', express.static('Main'));
+app.use('/static', express.static(path.join(__dirname, 'Main')))
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/Main/Home.html'));
+});
 
 app.listen(process.env.PORT || 8000, function(){
     console.log('Connected to localhost:8000');
